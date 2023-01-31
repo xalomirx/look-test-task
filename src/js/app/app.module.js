283f1237e597db916831065a468ce7eec55b6619
Mark Kravchenko <xalomirx@gmail.com>
2023-01-31 17:20:22 +0300

    made sidebar-view

--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -25,24 +25,22 @@ angular.module("app", ["templates"])
     };
 
     function contentViewCtrl($scope) {
-      $scope.filter = {
+      $scope.model.filters = {
         currentOrder: 'title',
         orderOptions: ['title', 'date'],
         isOnlyDate: false,
         searchQuery: '',
       }
 
-      $scope.newItemTitle = '';
+      $scope.model.newItemTitle = '';
 
       $scope.getCurrentDateFormat = () => {
-        const isOnlyDate = $scope.filter.isOnlyDate;
+        const isOnlyDate = $scope.model.filters.isOnlyDate;
         return isOnlyDate ? 'dd.mm.yyyy' : 'dd.mm.yyyy h:mm';
       }
 
       $scope.addNewItem = () => {
-        const newItemTitle = $scope.newItemTitle;
-
-        if (!newItemTitle.length) return;
+        const newItemTitle = $scope.model.newItemTitle;
 
         const newItem = {
           id: makeDataId(),
@@ -52,24 +50,66 @@ angular.module("app", ["templates"])
         }
 
         $scope.model.items.push(newItem);
-        $scope.newItemTitle = '';
+        $scope.model.newItemTitle = '';
       }
 
       $scope.selectItem = (id) => {
-        $scope.selectedItemId = id;
+        $scope.model.selectedItemId = id;
       }
 
       $scope.isSelectedItem = (id) => {
-        return $scope.selectedItemId === id;
+        return $scope.model.selectedItemId === id;
       }
     }
   })
   .directive("sidebarView", () => {
     return {
-      scope: {},
+      scope: {
+        model: "="
+      },
       restrict: "E",
       templateUrl: "./js/app/sidebar-view.tpl.html",
+      controller: ['$scope', sidebarViewCtrl],
+      controllerAs: 'SC',
     };
+
+    function sidebarViewCtrl($scope) {
+      $scope.model.currentItem = undefined;
+      $scope.model.newTagTitle = '';
+
+      const findCurrentItem = () => {
+        const items = $scope.model.items;
+        const currentItemId = $scope.model.selectedItemId;
+
+        return items.find(item => item.id === currentItemId);
+      }
+
+      const findCurrentItemIndex = () => {
+        const currentItemId = $scope.model.selectedItemId;
+        const items = $scope.model.items;
+        return items.findIndex(item => item.id === currentItemId);
+      }
+
+      const setCurrentItem = () => {
+        const currentItemId = $scope.model.selectedItemId;
+        if (currentItemId) $scope.model.currentItem = findCurrentItem();
+      }
+
+      $scope.addNewTag = () => {
+        const newTagTitle = $scope.model.newTagTitle;
+        const currentItemIndex = findCurrentItemIndex();
+
+        $scope.model.items[currentItemIndex].tags.push(newTagTitle);
+        $scope.model.newTagTitle = '';
+      }
+
+      $scope.$watch('model.selectedItemId', setCurrentItem);
+
+      $scope.removeTag = (index) => {
+        const currentItemIndex = findCurrentItemIndex();
+        $scope.model.items[currentItemIndex].tags.splice(index, 1);
+      }
+    }
   })
   .directive("elementsView", () => {
     return {
